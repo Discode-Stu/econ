c3bb94cbd401bf16ab62cf3c7b4cae239d2b38bc
Discode Stu <dstuart85@yahoo.com>
2020-07-21 12:17:12 -0400

    purchase detail component

--- src/actions/user.js
+++ src/actions/user.js
@@ -19,7 +19,7 @@ export function fetchUserPurchases() {
                 _id: 0,
                 total: 8.34,
                 orderNumber: 'A00469246',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jordan Hudgens',
@@ -30,7 +30,7 @@ export function fetchUserPurchases() {
                 _id: 1,
                 total: 9.35,
                 orderNumber: 'A0435gffthh',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Dan Stuart',
@@ -41,7 +41,7 @@ export function fetchUserPurchases() {
                 _id: 2,
                 total: 11.44,
                 orderNumber: 'A048rhfkeih',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Krista Swanson',
@@ -52,7 +52,7 @@ export function fetchUserPurchases() {
                 _id: 3,
                 total: 8.38,
                 orderNumber: 'G4567lkj2',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Chad Brookes',
@@ -63,7 +63,7 @@ export function fetchUserPurchases() {
                 _id: 4,
                 total: 3.94,
                 orderNumber: 'Belihl1246',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Thomas Stewart',
@@ -74,7 +74,7 @@ export function fetchUserPurchases() {
                 _id: 5,
                 total: 6.45,
                 orderNumber: 'Dlk4958700f',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Dana Stack',
@@ -85,7 +85,7 @@ export function fetchUserPurchases() {
                 _id: 6,
                 total: 18.34,
                 orderNumber: 'A006542l',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'James Hemperton',
@@ -96,7 +96,7 @@ export function fetchUserPurchases() {
                 _id: 7,
                 total: 8.34,
                 orderNumber: 'Ay04726',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Ian Grassfed',
--- src/components/account/purchaseDetail.js
+++ src/components/account/purchaseDetail.js
@@ -1,15 +1,73 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
+function PurchaseDetailLabel({className, title, value}) {
+    return (
+        <div className={`${className} purchase-detail-label`}>
+            <div className='purchase-detail-label__title'>{title}</div>
+            <div className='purchase-detail-label__value'>{value}</div>
+        </div>
+    )
+}
+
 class PurchaseDetail extends Component {
     render() {
-        const { className } = this.props;
+        const { className, orderNumber, orderDate, user, total, creditCard } = this.props;
+        const { name, shippingAddress } = user;
 
         return (
             <div className={`${className} purchase-detail`}>
-                purchase detail goes here
+                <PurchaseDetailLabel 
+                className='purchase-detail__order-number' 
+                title='Order Number'
+                value={orderNumber}
+                />
+                <PurchaseDetailLabel 
+                className='purchase-detail__order-date' 
+                title='Order Date'
+                value={orderDate}
+                />
+                <PurchaseDetailLabel 
+                className='purchase-detail__shipping' 
+                title='Shipping Address'
+                value={`${name}\n${shippingAddress}`}
+                />
+                <PurchaseDetailLabel 
+                className='purchase-detail__total' 
+                title='Total'
+                value={total}
+                />
+                <PurchaseDetailLabel 
+                className='purchase-detail__credit-card' 
+                title='Credit Card'
+                value={creditCard}
+                />               
             </div>
+
+
+
+// {
+//     _id: 0,
+//     total: 8.34,
+//     orderNumber: 'A00469246',
+//     orderDate: new Date(),
+//     creditCard: '-0000',
+//     user: {
+//         name: 'Jordan Hudgens',
+//         shippingAddress: '1234 West State St'
+//     }
         )
     }
 }
+
+function mapStateToProps(state) {
+    const { purchaseDetail } = state.user;
+    return {
+        ...purchaseDetail
+    }
+}
+
+PurchaseDetail = connect(mapStateToProps)(PurchaseDetail);
 
 export default PurchaseDetail;
