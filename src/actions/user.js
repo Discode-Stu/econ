b8773b8976f4611057acedaf286777069560cc89
Discode Stu <dstuart85@yahoo.com>
2020-08-07 14:49:08 -0400

    added sign in functionality

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,8 +2,26 @@ import {
     SET_USER_PURCHASES,
     SET_PURCHASE_DETAIL,
     SET_CART_PRODUCTS,
-    ADD_CART_PRODUCT
+    ADD_CART_PRODUCT,
+    AUTHENTICATE_USER
+
 } from './types';
+
+export function signIn({email, password}) {
+    console.log({email, password});
+    
+    return ({
+        type: AUTHENTICATE_USER,
+        payload: {
+            user: {
+                _id: 0,
+                name: 'Dan Stuart',
+                address: '1223 address st',
+                cartProducts: []
+            }
+        }
+    })
+}
 
 export function setPurchaseDetail (_id) {
     return ({
